c55e0878cebdeb7a3b18edb03afde8f9a549225c
James Matthew Temple <jmtemple0429@gmail.com>
2025-10-05 20:01:55 -0400

    add fingerprint sdk

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
+use Fingerprint\ServerAPI\Api\FingerprintApi;
+use Fingerprint\ServerAPI\Configuration;
+use GuzzleHttp\Client;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -26,6 +29,25 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
+
+$config = Configuration::getDefaultConfiguration(
+  config('services.fingerprint.api_key'),
+  Configuration::REGION_GLOBAL // Replace with your region
+);
+
+$client = new FingerprintApi(
+  new Client(),
+  $config
+);
+
+// Get a specific identification event
+try {
+  list($model, $response) = $client->getEvent("REQUEST_ID");
+  echo "Status: " . $response_upd->getStatusCode();
+  echo "<pre>" . $model . "</pre>";
+} catch (Exception $e) {
+  echo $e->getMessage(), PHP_EOL;
+}
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
--- config/services.php
+++ config/services.php
@@ -18,6 +18,10 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    'fingerprint' => [
+        'api_key' => env('FINGERPRINT_API_KEY'),
+    ],
+
     'resend' => [
         'key' => env('RESEND_KEY'),
     ],
